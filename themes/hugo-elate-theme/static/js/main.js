ab67cb6ef0e46e7043eccbcc6dbddbfb320f8449
mca91 <martin.emwikts@googlemail.com>
2021-05-02 19:45:12 +0200

    new FAQ, style update

--- themes/hugo-elate-theme/static/js/main.js
+++ themes/hugo-elate-theme/static/js/main.js
@@ -280,7 +280,7 @@
 	};
 
 	var servicesAnimate = function() {
-		var services = $('#fh5co-services');
+		var services = $('#fh5co-application');
 		if ( services.length > 0 ) {	
 
 			services.waypoint( function( direction ) {
